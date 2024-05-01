719f4e089592dbafd3a197c4336cc60538eda60b
eloiJhn <eloi.jahan9221@gmail.com>
2024-05-01 16:57:37 +0200

    feat: :art: Setting up the first version of the title

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,17 @@
+// pages/index.js
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold">Bienvenue sur notre site</h1>
-      <section className="my-8">
-        <h2 className="text-3xl font-bold">Historique de l'appartement</h2>
-        <p>Texte détaillant l'historique de l'appartement...</p>
-      </section>
-      <section className="my-8">
-        <h2 className="text-3xl font-bold">Photos</h2>
-        <div className="grid grid-cols-3 gap-4">
-        </div>
-      </section>
-      <section className="my-8">
-        <h2 className="text-3xl font-bold">Contactez-nous</h2>
-        <form>
-        </form>
-      </section>
+    <div className="min-h-screen" style={{ background: "linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%)" }}>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Merienda+One&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="pt-8 text-center px-4">
+        <h1 className="text-6xl font-bold" style={{ fontFamily: "'Merienda One', cursive", color: "#333" }}>Au Roi Lion</h1>
+        {/* Inclure d'autres éléments ici, comme des images ou des informations */}
+      </div>
     </div>
   );
 }
